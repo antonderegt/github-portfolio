31a694b08216fc236b1af6f03e26b8d28a56ade9
Anton de Regt <antonderegt@pm.me>
2019-10-22 20:39:34 +0200

    First commit

---
+++ src/components/PrimaryButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>{{ value }}</h2>
+    <h1>{{ count }}</h1>
+    <button class="bg-purple-600 text-green-600" @click="count++">Add</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: String
+  },
+  data: () => {
+    return {
+      count: 0
+    };
+  }
+}
+</script>
+
+<style scoped>
+</style>
