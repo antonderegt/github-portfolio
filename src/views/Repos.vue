0ceae4f832ca5c7e1bb39e01d1e6e15e0124832b
Anton de Regt <antonderegt@pm.me>
2019-10-25 18:56:31 +0200

    Fixes typo in loopup button

--- src/views/Repos.vue
+++ src/views/Repos.vue
@@ -33,7 +33,7 @@
           <div class="flex items-center justify-between">
             <router-link :to="/repos/+username">
               <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-                Look up 
+                Lookup 
               </button>
             </router-link>
           </div>
