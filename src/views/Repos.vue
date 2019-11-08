4e6f5c04ff7c5519e5c5ab832fcf4aa317124d0b
Anton de Regt <antonderegt@pm.me>
2019-11-08 17:00:52 +0100

    Updates style of input field

--- src/views/Repos.vue
+++ src/views/Repos.vue
@@ -1,43 +1,32 @@
 <template>
   <div>
-    <h1 class="text-xl text-gray-600 leading-tight mb-5">Repos</h1>
-    <!-- <router-link class="bg-gray-600 text-white rounded-full no-underline px-4 py-2 mx-4" to="/products/2">2</router-link>
-    <router-link class="bg-gray-600 text-white rounded-full no-underline px-4 py-2 mx-4" to="/products/7">7</router-link>
-    <router-link class="bg-gray-600 text-white rounded-full no-underline px-4 py-2 mx-4" to="/products/4">4</router-link> -->
-    <div v-if="user != null" class="flex flex-col">
-      <div v-for="repo in repos" :key="repo.node_id" class="flex m-5 p-5 rounded shadow-lg">
-        <div class="flex-2">
-          <img class="h-20 rounded-full m-5" :src="repo.owner.avatar_url" >
-        </div>
-        <div class="flex-1">
-          <p>{{user}}</p>
-          <a :href="repo.html_url" class="font-bold text-xl mb-2 text-blue-500" target="_blank">{{ repo.full_name }}</a>
-          <p>{{repo.description}}</p>
-          <div class="px-6 py-4">
-            <span class="m-2"><span class="inline-block bg-red-600 rounded-full px-3 py-1 text-sm font-semibold text-white">{{repo.open_issues}}</span><span class="ml-1">Issues</span></span>
-            <span class="m-2"><span class="inline-block bg-red-600 rounded-full px-3 py-1 text-sm font-semibold text-white">{{repo.watchers}}</span><span class="ml-1">Watchers</span></span>
-            <span class="m-2"><span class="inline-block bg-red-600 rounded-full px-3 py-1 text-sm font-semibold text-white">{{repo.forks}}</span><span class="ml-1">Forks</span></span>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div v-else class="flex flex-col items-center">
+    <div class="flex flex-col items-center">
       <div class="w-full max-w-xs">
-        <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-          <div class="mb-4">
-            <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
-              Username
-            </label>
-            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" v-model="username" id="username" type="text" placeholder="Username">
-          </div>
-          <div class="flex items-center justify-between">
+        <form class="w-full max-w-sm">
+          <div class="flex items-center border-b border-b-2 border-teal-500 py-2">
+            <input v-model="username" class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Username" aria-label="Username">
             <router-link :to="/repos/+username">
-              <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-                Lookup 
-              </button>
+              <button class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="button">Lookup</button>
             </router-link>
           </div>
         </form>
+      </div>
+      <div v-if="user != null" class="flex flex-col">
+        <div v-for="repo in repos" :key="repo.node_id" class="flex m-5 p-5 rounded shadow-lg">
+          <div class="flex-2">
+            <img class="h-20 rounded-full m-5" :src="repo.owner.avatar_url" >
+          </div>
+          <div class="flex-1">
+            <p>{{user}}</p>
+            <a :href="repo.html_url" class="font-bold text-xl mb-2 text-blue-500" target="_blank">{{ repo.full_name }}</a>
+            <p>{{repo.description}}</p>
+            <div class="px-6 py-4">
+              <span class="m-2"><span class="inline-block bg-red-600 rounded-full px-3 py-1 text-sm font-semibold text-white">{{repo.open_issues}}</span><span class="ml-1">Issues</span></span>
+              <span class="m-2"><span class="inline-block bg-red-600 rounded-full px-3 py-1 text-sm font-semibold text-white">{{repo.watchers}}</span><span class="ml-1">Watchers</span></span>
+              <span class="m-2"><span class="inline-block bg-red-600 rounded-full px-3 py-1 text-sm font-semibold text-white">{{repo.forks}}</span><span class="ml-1">Forks</span></span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
