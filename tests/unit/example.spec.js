993234d8ec9b969b092a0fdd41edf09ba283d656
Anton de Regt <antonderegt@pm.me>
2019-11-11 19:31:03 +0100

    Adds unit tests for Repos.vue

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,5 +1,6 @@
 import { shallowMount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
+import Repos from '@/views/Repos.vue'
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
@@ -8,5 +9,21 @@ describe('HelloWorld.vue', () => {
       propsData: { msg }
     })
     expect(wrapper.text()).toMatch(msg)
+  })
+})
+
+describe('Repos.vue', () => {
+  // Inspect the raw component options
+  it('has created function selectTab', () => {
+    expect(typeof Repos.methods.getData).toBe('function')
+  })
+
+  // Evaluate the results of functions in
+  // the raw component options
+  it('sets the correct default data', () => {
+    expect(typeof Repos.data).toBe('function')
+    const defaultData = Repos.data()
+    expect(defaultData.username).toBe('')
+    expect(defaultData.repos).toBe(null)
   })
 })
